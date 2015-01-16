35a564ceca018efd1b2c2335f3be3dc7e21e6a1b
sneakernets <consigno@soulsphere.org>
2015-01-16 12:50:27 -0600

    Adding v_text.cpp

--- src/v_text.cpp
+++ src/v_text.cpp
@@ -149,17 +149,9 @@ void DCanvas::DrawTextV(FFont *font, int normalcolor, int x, int y, const char *
 		// We don't handle these. :(
 		case DTA_DestWidth:
 		case DTA_DestHeight:
-			assert(false && "unsupported");
-			//*(DWORD *)tags = TAG_IGNORE;
-			data = va_arg (tags, DWORD);
-			break;
-
-		// Translation is specified explicitly by the text.
 		case DTA_Translation:
-			assert(false && "unsupported");
-			//*(DWORD *)tags = TAG_IGNORE;
-			ptrval = va_arg (tags, void*);
-			break;
+			assert("Bad parameter for DrawText" && false);
+			return;
 
 		case DTA_CleanNoMove_1:
 			boolval = va_arg (tags, INTBOOL);
